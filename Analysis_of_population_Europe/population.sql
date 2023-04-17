05d380a21f639b229fbcefe4bf05f4286774817f
Jola2 <100040541+Jola2@users.noreply.github.com>
2023-04-17 21:15:14 +0200

    Update population.sql

--- Analysis_of_population_Europe/population.sql
+++ Analysis_of_population_Europe/population.sql
@@ -5,7 +5,7 @@ CREATE TABLE population
   country_name VARCHAR(50),
   region VARCHAR(50),
   area VARCHAR(40),
-  population numeric,
+  population_country numeric,
   population_per_sq_km numeric,
   male_life_expectancy numeric,
   female_life_expectancy numeric,
